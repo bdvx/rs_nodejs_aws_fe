70aa33bbd82099deb4f51b2bbf8bf1bff5285dfc
Denis Beskorovainyi <74551976+DBeskorovainyiEPAM@users.noreply.github.com>
2022-08-22 02:10:28 +0400

    Task 9

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,10 @@
+
+const API_PATHS = {
+  product: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://77upjvo4f6.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://755vvfmegj.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://bdvx-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+};
+
+export default API_PATHS;
